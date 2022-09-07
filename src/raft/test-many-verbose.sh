a7dd52f87e5c83c153d206fdc8d648707c32cb64
Zhangwanghe <13384228948@163.com>
2022-09-07 15:00:48 +0800

    modfiy test script

--- src/raft/test-many-verbose.sh
+++ src/raft/test-many-verbose.sh
@@ -1,7 +1,27 @@
+#!/usr/bin/env bash
+
 rm outputDir -rf
 mkdir outputDir
 
-for i in $(seq 1 100); do
+SC=0
+for i in $(seq 1 500); do
     output="outputDir/output_${i}.txt"
-    VERBOSE=1 go test -run TestManyElections2A -race  >> ${output}
+    VERBOSE=1 go test -run 2A -race  >> ${output}
+    sleep 2
+    VERBOSE=1 go test -run 2B -race  >> ${output}
+    sleep 2
+    VERBOSE=1 go test -run 2C -race  >> ${output}
+    sleep 5
+    VERBOSE=1 go test -run 2D -race  >> ${output}
+    sleep 5
+    
+
+    NT=`cat ${output} | grep FAIL | wc -l`
+    if [ "$NT" -eq "0" ]
+    then
+    rm ${output} -rf
+    let SC++
+    fi
 done
+
+echo "$SC" success
