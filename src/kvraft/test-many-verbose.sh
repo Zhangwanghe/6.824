d9a40d07b8c33fb38b2e8d4362ee161f57743eea
Zhangwanghe <13384228948@163.com>
2022-09-13 15:34:25 +0800

    mdoify test script

--- src/kvraft/test-many-verbose.sh
+++ src/kvraft/test-many-verbose.sh
@@ -3,7 +3,18 @@
 rm outputDir -rf
 mkdir outputDir
 
+SC=0
 for i in $(seq 1 100); do
     output="outputDir/output_${i}.txt"
-    go test -run TestConcurrent3A -race  >> ${output}
+    VERBOSE=1 go test -run TestSnapshotRecoverManyClients3B -race  >> ${output}
+    sleep 2
+
+    NT=`cat ${output} | grep FAIL | wc -l`
+    if [ "$NT" -eq "0" ]
+    then
+    rm ${output} -rf
+    let SC++
+    fi
 done
+
+echo "$SC" success
