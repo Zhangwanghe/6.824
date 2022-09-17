04598e784d54b633c929b68d08767333872cbdc8
Zhangwanghe <13384228948@163.com>
2022-09-17 11:18:21 +0800

    add test script

---
+++ src/shardctrler/test-many.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+output="output_many.log"
+
+rm ${output}
+touch ${output}
+
+for i in $(seq 1 100); do
+    go test -race >> ${output}
+done
+
+NT=`cat ${output} | grep FAIL | wc -l`
+if [ "$NT" -eq "0" ]
+then
+"Passed All"
+fi
